5ae6fe724d7dab5eaf834f3d18bcde03319318dc
KrishnaPassi1818 <krishnapassi92@gmail.com>
2026-04-13 16:09:47 +0530

    fixing top banner and adding features

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 $(".price-btn").click(() => {
     var choice = confirm(" Do you really want to get started?");
     if (choice == 1) {
-      window.location.href = "/html/pricing.html"
+      window.location.href = "/html/pricing.html";
     }
 });
 
 $("#p-btn-2").click( () => {
     $("#freePlan").html("<h2>$0<sub>/month</sub></h2>");
     $("#paidPlan").html("<h2>$249<sub>/month</sub></h2>");
-})
+});
 $("#p-btn-1").click( () => {    
     $("#freePlan").html("<h2>$0<sub>/month</sub></h2>");
     $("#paidPlan").html("<h2>$29<sub>/month</sub></h2>");
-})
+});
 
+$(".image").click(() => {
+  window.location.href = "/html/about.html";
+});
 
 const faqBtns = document.querySelectorAll(".faq-container img");
 const faqCloseBtns = document.querySelectorAll(".faqCloseBtn");
--- script/pricing.js
+++ script/pricing.js
@@ -23,3 +23,26 @@ $("#btn4").click( () => {
         'background': '#ffffff'
     });
 });
+
+const faqBtns = document.querySelectorAll(".faq-container img");
+const faqCloseBtns = document.querySelectorAll(".faqCloseBtn");
+
+faqBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const faqModal = btn.nextElementSibling;
+    faqModal.classList.add("show");
+  });
+});
+
+faqCloseBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const faqModal = btn.closest(".faqans");
+    faqModal.classList.remove("show");
+  });
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("faqans")) {
+    e.target.classList.remove("show");
+  }
+});
